8647235ce2d1f5ff13c6dc3134a4c621e677e25f
boomieindahouse <prawitsanat@hotmail.com>
2025-01-06 22:54:46 +0700

    edit tabs color

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           <h1 class="text-xl font-bold text-white">Home</h1>
         </div>
         <div class="tabs flex">
-          <button class="flex-1 px-8 py-2 font-semibold border-b-4 border-primary">For you</button>
+          <button class="flex-1 px-8 py-2 font-semibold border-b-4 text-white border-primary">For you</button>
           <button class="flex-1 px-8 py-2 text-gray-500 hover:bg-gray-50 dark:hover:bg-gray-900">Following</button>
         </div>
       </div>
